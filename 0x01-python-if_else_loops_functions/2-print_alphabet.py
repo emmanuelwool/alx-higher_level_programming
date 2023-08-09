1e2ec3d450ab66b1d43c44daff658b05c513e8db
emmanuelwool <emmanuelwoollock@gmail.com>
2023-08-09 16:16:44 -0700

    alphabet in lower

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-for char in range(26):
-    if char != 4 and char != 16:
-        print("{:s}".format(chr(char + ord("a"))), end="")
+for alphabet in range(97, 123):
+    print("{}".format(chr(alphabet)), end="")
