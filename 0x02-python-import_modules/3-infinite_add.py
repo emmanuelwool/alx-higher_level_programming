3ba5d9bb3df48aa1290c954a208ffecdc8f8dbc3
emmanuelwool <emmanuelwoollock@gmail.com>
2023-08-11 15:19:05 -0700

    Infinite addition

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -7,5 +7,5 @@ if __name__ == "__main__":
     count = len(sys.argv) - 1
     sum=0
     for i in range(count):
-        sum=sum+i
-print("sum of arg is {}".format(sum))
+        sum=sum+int(sys.argv[i + 1])
+print("{}".format(sum))
