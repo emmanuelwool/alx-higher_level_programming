ac8b358007706d8998486620a202d32dc97793f2
emmanuelwool <emmanuelwoollock@gmail.com>
2023-08-14 14:58:37 -0700

    delete t

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    new_list=my_list.remove(idx)
-    return new_list
+    if idx < 0 or (len(my_list) - 1):
+        return my_list
+    else:
+        new_list=my_list.remove(idx)
+        return new_list
